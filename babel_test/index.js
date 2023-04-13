87ea6d43ca3c9701cdba9cbaa0e634f2e02342b7
이유찬 <citron03@dreamsecurity.com>
2023-04-13 17:43:00 +0900

    set babel output script

--- babel_test/index.js
+++ babel_test/index.js
@@ -3,6 +3,18 @@ import moment from "moment";
 
 const outputPath = path.join(__dirname, "dist");
 
-console.log(`time stamp ${moment().format()} - "${outputPath}"`);
+// console.log(`time stamp ${moment().format()} - "${outputPath}"`);
+
+class time {
+  constructor() {
+    this.now = moment().format();
+  }
+  getTime() {
+    return this.now;
+  }
+}
+
+const t = new time();
+console.log("time", t.getTime());
 
 export { outputPath };
